6f5cc60b0e26a6ce4a65446ffbde8b5f83005691
miryambathilde <mbathilde@gmail.com>
2021-04-21 15:27:44 +0200

    asignamos audio con metodo aplicarSonido + estilo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,4 +7,15 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'Piano';
+
+  /* metodo aplicarSonido */
+  aplicarSonido(numero: number): void{
+    const audio = new Audio();
+    /* ruta relativa para que coja el numero de nota segun la tecla */
+    audio.src='../assets/sonidos/note' + numero + '.wav';
+    /* que cargue el audio */
+    audio.load();
+    /* que ejecute el audio */
+    audio.play();
+  }
 }
